7afb59f8286fcfae67229a4dfdc1339817dd3643
Jayden Stone <220652513+JaydenBStone@users.noreply.github.com>
2025-09-01 11:54:14 +1000

    Added Parent Container in HTML Added two divs using DOM Manip  - these divs will be the grid and the control panel for the etch a sketch Styled them with solid backgrounds to make them easier to visualise for the time being until i get their final size

---
+++ script.js
@@ -0,0 +1,21 @@
+// DOM
+
+const etchASketch = document.querySelector('.etchASketch');
+
+// Grid Container DOM
+const gridContainer = document.createElement('div');
+gridContainer.classList.add('gridContainer')
+etchASketch.append(gridContainer);
+
+
+// Control Panel DOM
+const controlPanel = document.createElement('div');
+controlPanel.classList.add('controlPanel')
+etchASketch.append(controlPanel);
+
+
+
+
+
+// Fucntions 
+
