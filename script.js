474936143a99c9796c25916e11e74538144cf7a4
Jayden Stone <220652513+JaydenBStone@users.noreply.github.com>
2025-09-01 12:05:20 +1000

    Added Header Div  - title and description within header

--- script.js
+++ script.js
@@ -2,15 +2,29 @@
 
 const etchASketch = document.querySelector('.etchASketch');
 
+// Header 
+const header = document.createElement('div');
+header.classList.add('header');
+etchASketch.append(header);
+// Title 
+const title = document.createElement('div');
+title.classList.add('title');
+header.append(title);
+// Description
+const description = document.createElement('div');
+description.classList.add('description');
+header.append(description);
+
+
 // Grid Container DOM
 const gridContainer = document.createElement('div');
-gridContainer.classList.add('gridContainer')
+gridContainer.classList.add('gridContainer');
 etchASketch.append(gridContainer);
 
 
 // Control Panel DOM
 const controlPanel = document.createElement('div');
-controlPanel.classList.add('controlPanel')
+controlPanel.classList.add('controlPanel');
 etchASketch.append(controlPanel);
 
 
